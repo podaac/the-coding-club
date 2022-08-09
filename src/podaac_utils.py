257dbf898adae6fd6823053eed37c758faca915c
Jinbo Wang <jinbow@gmail.com>
2022-08-09 11:33:18 -0700

    add src and docs

---
+++ src/podaac_utils.py
@@ -0,0 +1,110 @@
+"""
+Some ground level functions
+"""
+
+import requests
+from pprint import pprint
+CMR_OPS = 'https://cmr.earthdata.nasa.gov/search'
+collection_url = 'https://cmr.earthdata.nasa.gov/search/collections'
+var_url = "https://cmr.earthdata.nasa.gov/search/variables"
+
+def find_dataset(provider='podaac',
+                 keywords=['swot','level-2']):
+    """
+    Find a list of collections/datasets that match all the keywords from the keywords list.
+    
+    
+    """
+    import pandas as pd
+
+    if 'podaac' in provider.lower().replace('.',''):
+        provider='POCLOUD'
+        
+    response = requests.get(collection_url,params={'cloud_hosted': 'True',
+                                        'has_granules': 'True',
+                                        'provider': provider,
+                                        'page_size':2000,},
+                                headers={'Accept': 'application/json', } )
+    
+    collections = response.json()['feed']['entry']
+    
+    entries={}
+    entries['short_name']=[]
+    entries['long_name']=[]
+    entries['concept_id']=[]
+    entries['time_start']=[]
+    entries['time_end']=[]
+    
+    
+    for collection in collections:
+        
+        title="%s %s %s"%(collection["short_name"],collection["dataset_id"][:97],collection["id"])
+        match=1
+        for kw in keywords:
+            match *= kw.lower() in title.lower()
+            
+        if match==1:
+            entries['short_name'].append(collection["short_name"])
+            entries['concept_id'].append(collection["id"])
+            entries['long_name'].append(collection["dataset_id"])
+            entries['time_start'].append(collection["time_start"])
+            try:
+                entries['time_end'].append(collection["time_end"])
+            except:
+                entries['time_end'].append(['NaT/Present'])
+    
+    return pd.DataFrame(entries)
+
+def all_pocloud_dataset(provider='podaac'):
+    """
+    a list of all POCLOUD collections.
+    """
+    import pandas as pd
+
+    if 'podaac' in provider.lower().replace('.',''):
+        provider='POCLOUD'
+        
+    response = requests.get(collection_url,params={'cloud_hosted': 'True',
+                                        'provider': provider,
+                                        'page_size':2000,},
+                                headers={'Accept': 'application/json', } )
+    
+    collections = response.json()['feed']['entry']
+    
+    entries={}
+    entries['short_name']=[]
+    entries['long_name']=[]
+    entries['concept_id']=[]
+    entries['time_start']=[]
+    entries['time_end']=[]
+    
+    
+    for collection in collections:
+        
+        title="%s %s %s"%(collection["short_name"],collection["dataset_id"][:97],collection["id"])
+        match=1
+        entries['short_name'].append(collection["short_name"])
+        entries['concept_id'].append(collection["id"])
+        entries['long_name'].append(collection["dataset_id"])
+        entries['time_start'].append(collection["time_start"][:10])
+        try:
+            entries['time_end'].append(collection["time_end"][:10])
+        except:
+            entries['time_end'].append(['NaT/Present'])
+    
+    return pd.DataFrame(entries)
+
+def direct_s3(provider='podaac'):
+    import requests,s3fs
+    s3_cred_endpoint = {
+        'podaac':'https://archive.podaac.earthdata.nasa.gov/s3credentials',
+        'lpdaac':'https://data.lpdaac.earthdatacloud.nasa.gov/s3credentials'}
+
+    temp_creds_url = s3_cred_endpoint[provider]
+    creds = requests.get(temp_creds_url).json()
+    s3 = s3fs.S3FileSystem(anon=False,
+                           key=creds['accessKeyId'],
+                           secret=creds['secretAccessKey'], 
+                           token=creds['sessionToken'])
+    return s3
+

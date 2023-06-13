645dc6cf8deb25fd24e4ca31008b470ea1a1eb99
Dean Henze <54649692+DeanHenze@users.noreply.github.com>
2023-06-13 13:41:22 -0700

    downscaling code in a .py file
    script includes a function to call with options for downscaling in parallel and number of files.

--- notebooks/EC2_performance_assessment/dscale_s3_dask.py
+++ notebooks/EC2_performance_assessment/dscale_s3_dask.py
@@ -18,11 +18,8 @@ import s3fs
 import requests
 
 import xarray as xr
-#import h5py
 import numpy as np
-import pandas as pd
 
-import dask
 import dask.array as da
 from dask.distributed import Client, LocalCluster, progress
 from dask import delayed
@@ -99,59 +96,51 @@ def downscale_xr_s3(fs_s3, filepath_s3, output_dir, chunks=None):
     
     sstdata.close()
     
+    
+
+def downscale_with_parallel(n_workers, threads_per_worker, n_files):
+    # Get temporary AWS credentials for access
+    fs_s3 = init_S3FileSystem()
+    
+    # File paths of all MUR granules:
+    s3path = "s3://podaac-ops-cumulus-protected/MUR-JPL-L4-GLOB-v4.1/"
+    fns = fs_s3.glob(s3path+"*.nc")
+    print("total granules = ",len(fns))
+
+    # Directory to place downscaled files in:
+    dir_downscaled = "./sst_downscaled/"
+    if not os.path.isdir(dir_downscaled):
+        os.mkdir(dir_downscaled)
+
+    # Start dask client:
+    client = Client(n_workers=n_workers, threads_per_worker=threads_per_worker)
+
+    # Parallel computation
+    delayed_downscale = delayed(downscale_xr_s3)
+    ddtasks = [
+        delayed_downscale(fs_s3, fn, dir_downscaled)
+        for fn in fns[:n_files]
+        ]
+
+        # time the computation:
+    tick = time.time()
+    _ = da.compute(*ddtasks)
+    tock = time.time()
+    comptime = str(int(tock-tick))
+    print("Total computation time =", comptime, "seconds.")
+
+    client.close()
 
 
-@dask.delayed
-def downscale_xr_s3_dd(fs_s3, filepath_s3, output_dir, chunks=None):
-    """
-    Open and downscale SST's from 1 km to 100 km from a MUR 1 km file on an S3 bucket. fs_s3 is the 
-    S3 file system and filepath_s3 is the MUR filepath on S3. output_dir is the directory to save 
-    the downscaled data file in.
-    """
+    # Save computation time results
+    fname_results = 'computation_results_'+str(round(time.time()))+'.csv'
+    with open(fname_results, 'a') as f:
+        f.write('n_workers,threads_per_worker,n_files,compute_time_seconds'+'\n')
+        f.write(str(n_workers)+','+str(threads_per_worker)+','+str(n_files)+','+comptime+'\n')
     
-    print('Opening ', filepath_s3)
-    s3_file_obj = fs_s3.open(filepath_s3, mode='rb')
-    sstdata = xr.open_dataset(s3_file_obj, chunks=chunks)
-    
-    def downscale_mursst(sst):
-        """
-        This function takes MUR SST 1km data (with dimensions of 17900x36000), reshapes, and 
-        computes the mean temperature in 1x1 degree boxes, and then returns the processed 
-        SST data (shape 180x360).
 
-        Parameters
-        ----------
-        sst: 2D array-like of sea surface temperatures.
-        """
-        sst0 = np.r_[sst[0:1, :], sst]; del sst
-        sst = sst0.reshape(180, 100, 360, 100).mean(axis=-1).mean(axis=1); del sst0
-        return sst
-    
-    print("Downscaling.")
-    sst_downscaled = downscale_mursst(sstdata['analysed_sst'][0, ...])
-    
-    # Save downscaled SST to .nc file:
-    print("Saving output.")
-    ds = xr.Dataset(
-        data_vars=dict(
-            sst=(["time", "lat", "lon"], np.expand_dims(sst_downscaled, axis=0)) # Add dim for time.
-            ),
-        coords=dict(
-            lon=sstdata['lon'].values[::100], # lons, lats in downscaled res.
-            lat=sstdata['lat'].values[::100],
-            time=sstdata["time"].values,
-            ),
-        attrs=dict(
-            description="SST downscaled from MUR 1 km file to 100 km resolution.",
-            units="K",
-            ),
-        )
-    ds.to_netcdf(output_dir + filepath_s3.split("/")[-1][:-3] + "_downscaled.nc")
-    
-    sstdata.close()
-    
-    
-    
+
+"""
 if __name__=="__main__":
     
     # Get temporary AWS credentials for access
@@ -161,33 +150,38 @@ if __name__=="__main__":
     s3path = "s3://podaac-ops-cumulus-protected/MUR-JPL-L4-GLOB-v4.1/"
     fns = fs_s3.glob(s3path+"*.nc")
     print("total granules = ",len(fns))
-    print("Example filename: ", fns[0])
     
     # Directory to place downscaled files in:
-    if not os.path.isdir("./sst_downscaled/"):
-        os.mkdir("./sst_downscaled/")
-    
-    # Test downscaling one file:
-    #downscale_xr_s3(fs_s3, fns[0], "./sst_downscaled/", chunks=None)
+    dir_downscaled = "./sst_downscaled/"
+    if not os.path.isdir(dir_downscaled):
+        os.mkdir(dir_downscaled)
     
     # Start dask client:
-    n_workers = multiprocessing.cpu_count()
-    print(n_workers)
+    n_cpu = multiprocessing.cpu_count()
+    print(n_cpu)
     
-    client = Client(n_workers=int(n_workers/2), threads_per_worker=1)
+    n_workers = int(n_cpu/2)
+    threads_per_worker = 2
+    client = Client(n_workers=n_workers, threads_per_worker=threads_per_worker)
     print(client)
     
-    # Run downscaling tasks using dask delayed:
-    dscale_tasks = []
-    chunks = {'time':1, 'lat':int(17999/3), 'lon':int(36000/3)}
-    for i in range(100):
-        dscale_tasks.append(downscale_xr_s3_dd(fs_s3, fns[i], "./sst_downscaled/", chunks=None))
-    print(dscale_tasks)
+    # Parallel computation
+    delayed_downscale = delayed(downscale_xr_s3)
+    ddtasks = [
+        delayed_downscale(fs_s3, fn, dir_downscaled) 
+        for fn in fns[:10]
+        ]
     
         # time the computation:
     tick = time.time()
-    _ = da.compute(*dscale_tasks)
+    _ = da.compute(*ddtasks)
     tock = time.time()
     print("Total computation time =", str(tock-tick), "seconds.")
-    
+
     client.close()
+
+
+    # Save computation time results
+    with open('computation_results.txt', 'a') as f:
+    f.write('Hello\n')
+"""
